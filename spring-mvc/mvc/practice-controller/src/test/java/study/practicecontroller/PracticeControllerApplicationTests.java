fcb68b33dd69f9535810edf89dbc831db373c410
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-16 10:37:54 +0900

    initial commit

---
+++ spring-mvc/mvc/practice-controller/src/test/java/study/practicecontroller/PracticeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.practicecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
