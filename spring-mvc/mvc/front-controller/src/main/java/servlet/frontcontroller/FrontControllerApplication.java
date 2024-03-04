de34ae56291352116c089a44e33b970dfbe5eb7d
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-04 22:09:48 +0900

    initial commit

---
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/FrontControllerApplication.java
@@ -0,0 +1,12 @@
+package servlet.frontcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontControllerApplication.class, args);
+	}
+}
---
+++ spring-mvc/mvc/front-controller/src/test/java/servlet/frontcontroller/FrontControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.frontcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
