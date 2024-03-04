08f338a5d064fa04494aa649cd8986e5fcc26323
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-04 22:34:49 +0900

    feat: 회원 저장 컨트롤러 - v1 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v1/controller/MemberSaveControllerV1.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -1,2 +1,29 @@
-package servlet.frontcontroller.v1.controller;public class MemberSaveControllerV1 {
+package servlet.frontcontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+import servlet.frontcontroller.v1.ControllerV1;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
 }
