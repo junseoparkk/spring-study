898a582b88928b4f958d5d3d320ec79e7c29c6e8
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-04 22:35:24 +0900

    feat: 회원 목록 조회 컨트롤러 - v1 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v1/controller/MemberListControllerV1.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -1,2 +1,26 @@
-package servlet.frontcontroller.v1.controller;public class MemberListControllerV1 {
+package servlet.frontcontroller.v1.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+import servlet.frontcontroller.v1.ControllerV1;
+
+public class MemberListControllerV1 implements ControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
 }
