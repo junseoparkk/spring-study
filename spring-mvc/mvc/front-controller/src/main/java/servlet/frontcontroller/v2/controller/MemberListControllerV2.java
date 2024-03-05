632807195a41bd64bbd80cb13c35c8bf9e310569
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 15:11:08 +0900

    feat: 회원 목록 조회 컨트롤러 - v2 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v2/controller/MemberListControllerV2.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -1,2 +1,22 @@
-package servlet.frontcontroller.v2.controller;public class MemberListControllerV2 {
+package servlet.frontcontroller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import servlet.frontcontroller.MyView;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+import servlet.frontcontroller.v2.ControllerV2;
+
+public class MemberListControllerV2 implements ControllerV2 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
 }
