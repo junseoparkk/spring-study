e1fb176a5ffc56378a411946554e001406386cee
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 16:08:48 +0900

    feat: 프론트 컨트롤러 - v3 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/FrontControllerServletV3.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/FrontControllerServletV3.java
@@ -1,4 +1,57 @@
 package servlet.frontcontroller.v3;
 
-public class FrontControllerV3 {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import servlet.frontcontroller.ModelView;
+import servlet.frontcontroller.MyView;
+import servlet.frontcontroller.v3.controller.MemberFormControllerV3;
+import servlet.frontcontroller.v3.controller.MemberListControllerV3;
+import servlet.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+    private final Map<String, ControllerV3> controllerStore = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        controllerStore.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerStore.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerStore.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV3 controller = controllerStore.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        // 논리 이름을 통해 뷰 리졸버가 해당 뷰를 반환
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
 }
