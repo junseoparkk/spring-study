214e0ecaa7296dbf44e9b8cffb2a8f5eaf3f07fc
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 15:56:20 +0900

    feat: 회원 목록 조회 컨트롤러 - v3 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/controller/MemberListControllerV3.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -1,2 +1,21 @@
-package servlet.frontcontroller.v3.controller;public class MemberListControllerV3 {
+package servlet.frontcontroller.v3.controller;
+
+import java.util.List;
+import java.util.Map;
+import servlet.frontcontroller.ModelView;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+import servlet.frontcontroller.v3.ControllerV3;
+
+public class MemberListControllerV3 implements ControllerV3 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+
+        return mv;
+    }
 }
