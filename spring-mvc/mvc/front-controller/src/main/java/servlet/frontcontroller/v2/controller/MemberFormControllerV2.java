c641eedb80930296d79315a04cbe2afdc6774245
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 15:10:49 +0900

    feat: 회원 조회 컨트롤러 - v2 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -1,2 +1,15 @@
-package servlet.frontcontroller.v2.controller;public class MemberFormControllerV2 {
+package servlet.frontcontroller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import servlet.frontcontroller.MyView;
+import servlet.frontcontroller.v2.ControllerV2;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
 }
