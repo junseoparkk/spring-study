cac33b7f7af916c0a4b40d43ee2dfc20400d1271
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 15:56:01 +0900

    feat: 회원 조회 컨트롤러 - v3 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -1,2 +1,12 @@
-package servlet.frontcontroller.v3.controller;public class MemberFormControllerV3 {
+package servlet.frontcontroller.v3.controller;
+
+import java.util.Map;
+import servlet.frontcontroller.ModelView;
+import servlet.frontcontroller.v3.ControllerV3;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
 }
