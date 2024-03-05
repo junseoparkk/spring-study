fb9fc28f980b540b1778297809650bd36d14ae28
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 19:18:18 +0900

    feat: 프론트 컨트롤러 - v5 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v5/FrontControllerServletV5.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/v5/FrontControllerServletV5.java
@@ -1,2 +1,79 @@
-package servlet.frontcontroller.v5;public class FrontControllerServletV5 {
+package servlet.frontcontroller.v5;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import servlet.frontcontroller.ModelView;
+import servlet.frontcontroller.MyView;
+import servlet.frontcontroller.v3.controller.MemberFormControllerV3;
+import servlet.frontcontroller.v3.controller.MemberListControllerV3;
+import servlet.frontcontroller.v3.controller.MemberSaveControllerV3;
+import servlet.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 핸들러 매핑
+        Object handler = findHandler(request);
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 핸들러를 처리할 수 있는 어댑터 조회
+        MyHandlerAdapter adapter = findHandlerAdapter(handler);
+
+        // 어댑터 호출
+        ModelView mv = adapter.handle(request, response, handler);
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyHandlerAdapter findHandlerAdapter(Object handler) {
+        MyHandlerAdapter adapter;
+        for (MyHandlerAdapter handlerAdapter : handlerAdapters) {
+            if (handlerAdapter.supports(handler)) {
+                return handlerAdapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter not found : " + handler);
+    }
+
+    private Object findHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
 }
