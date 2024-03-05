647e050e9b2ef3db8ef1f777cab9d2a34168ad22
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 21:50:43 +0900

    feat: 스프링 컨트롤러 - v3 구현

---
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,41 @@
+package servlet.frontcontroller.springmvc.v3;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @RequestMapping("/save")
+    public String save(
+            @RequestParam(name = "userName") String userName,
+            @RequestParam(name = "age") int age,
+            Model model
+    ) {
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    @RequestMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
