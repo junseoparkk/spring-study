57041fd843145f262e71881f0987fe7b792dc083
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-03-05 21:45:40 +0900

    feat: 스프링 컨트롤러 - v2 구현

--- spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/springmvc/v2/SpringMemberControllerV2.java
+++ spring-mvc/mvc/front-controller/src/main/java/servlet/frontcontroller/springmvc/v2/SpringMemberControllerV2.java
@@ -1,2 +1,44 @@
-package servlet.frontcontroller.springmvc.v2;public class SpringMemberControllerV2 {
+package servlet.frontcontroller.springmvc.v2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import servlet.frontcontroller.domain.Member;
+import servlet.frontcontroller.domain.MemberRepository;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
 }
